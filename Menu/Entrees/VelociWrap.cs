f535b59132413e45a82f505d574efd74c50e8120
unknown <dannielchristensen1@gmail.com>
2019-09-14 20:06:27 -0500

    Completed Tests

--- Menu/Entrees/VelociWrap.cs
+++ Menu/Entrees/VelociWrap.cs
@@ -4,18 +4,18 @@ namespace DinoDiner.Menu.Entrees
 {
     public class VelociWrap
     {
-        public double price { get; set; }
-        public uint calories { get; set; }
+        public double Price { get; set; }
+        public uint Calories { get; set; }
         private bool dressing = true;
         private bool lettuce = true;
         private bool cheese = true;
 
-        public List<string> ingredients
+        public List<string> Ingredients
         {
             get
             {
-                List<string> s = new List<string>() { "Flour Tortilla", "Chicken" };
-                if (cheese) s.Add("Parmesan");
+                List<string> s = new List<string>() { "Flour Tortilla", "Chicken Breast" };
+                if (cheese) s.Add("Parmesan Cheese");
                 if (dressing) s.Add("Ceasar Dressing");
                 if (lettuce) s.Add("Romaine Lettuce");
 
@@ -23,9 +23,13 @@ namespace DinoDiner.Menu.Entrees
             }
 
         }
-
-        public void holdLettuce() => this.lettuce = false;
-        public void holdCheese() => this.cheese = false;
-        public void holdDressing() => this.dressing = false;
+        public VelociWrap()
+        {
+            this.Price = 6.86;
+            this.Calories = 356;
+        }
+        public void HoldLettuce() => this.lettuce = false;
+        public void HoldCheese() => this.cheese = false;
+        public void HoldDressing() => this.dressing = false;
     }
 }
