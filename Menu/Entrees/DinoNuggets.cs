0c32f8e8272496ddb4305a9fdba35e0d31c11bf9
unknown <dannielchristensen1@gmail.com>
2019-09-14 19:40:06 -0500

    Adding DinoNuggets.cs

---
+++ Menu/Entrees/DinoNuggets.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+
+namespace DinoDiner.Menu.Entrees
+{
+    public class DinoNuggets{
+        public  int numNugs = 6;
+        public double price { get; set; }
+        public uint calories { get; set; }
+
+        public List<string> Ingredients
+        {
+            get
+            {
+                List<string> ingredients = new List<string>() { "6 Nuggets" };
+                return ingredients;
+            }
+        }
+
+        public DinoNuggets()
+        {
+            this.price = 4.25;
+            this.calories = numNugs * 59;
+        }
+
+        //Adds a single nugget -- can be overriden to take a certain num of extra nugs
+        public void addNugg()
+        {
+            this.numNugs++;
+            this.price += .25;
+            this.calories = numNugs * 59;
+        }
+
+        public void addNugg(int a)
+        {
+            this.numNugs += a;
+            this.price += (.25 * a);
+            this.calories = numNugs * 59;
+        }
+    }
+
+}
