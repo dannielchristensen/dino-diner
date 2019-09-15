96799593b8766ce49a5dde0f5d93e0b04751ee90
unknown <dannielchristensen1@gmail.com>
2019-09-14 19:40:29 -0500

    Adding PterodactylWings.cs

---
+++ Menu/Entrees/PterodactylWings.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace DinoDiner.Menu.Entrees
+{
+    public class PterodactylWings
+    {
+        public double price { get; set; }
+        public uint calories { get; set; }
+        public List<string> Ingredients
+        {
+            get
+            {
+                List<string> ingredients = new List<string>() { "Chicken", "Wing Sauce" };
+                return ingredients;
+            }
+        }
+
+        public PterodactylWings()
+        {
+            this.price = 7.21;
+            this.calories = 318;
+        }
+    }
+}
