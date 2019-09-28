752b6128d86803ef531131dbc86f1e51d5f4e425
unknown <dannielchristensen1@gmail.com>
2019-09-28 14:57:16 -0500

    Added New File and Removed extra files

---
+++ MenuTest/SodasaurusTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using DinoDiner.Menu.Drinks;
+
+namespace MenuTest
+{
+    class SodasaurusTest
+    {
+        [Fact]
+        public void HasCorrectDefaultCalories()
+        {
+            Sodasaurus s = new Sodasaurus();
+            Assert.Equal<uint>(112, s.Calories);
+
+        }
+        //The correct default Calories
+        // The correct default price
+        //the correct default ice
+        // the correct default size
+
+        //correct calories after setting to small
+        //correct price after setting small
+
+        //After setting medium
+
+        //After setting large
+    }
+}
