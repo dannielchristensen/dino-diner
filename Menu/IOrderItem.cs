ab80e0c73cbabcc8e5b5105eb77aa9463610cf4b
unknown <dannielchristensen1@gmail.com>
2019-10-17 15:34:38 -0500

    Files that needed to be added??

---
+++ Menu/IOrderItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DinoDiner.Menu
+{
+    public interface IOrderItem
+    {
+        double Price { get; set;  }
+        string Description { get; }
+
+        List<string> Special { get;  }
+    }
+}
---
+++ PointOfSale/MenuCategorySelection.xaml.cs
@@ -0,0 +1,74 @@
+﻿/* MainPage.xaml.cs
+*   Author: Danniel Christensen
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PointOfSale
+{
+    /// <summary>
+    /// Interaction logic for Page1.xaml
+    /// </summary>
+    public partial class Page1 : Page
+    {
+        /// <summary>
+        /// creates main page
+        /// </summary>
+        public Page1()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        /// controls Entree button
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Entrees_Click(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Navigate(new Uri("EntreeSelection.xaml", UriKind.Relative));
+            
+        }
+        /// <summary>
+        /// Controls Combo button
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Combo_Click(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Navigate(new ComboSelection());
+
+        }
+        /// <summary>
+        /// Controls drinks button
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Drinks_Click(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Navigate(new Uri("DrinkSelection.xaml", UriKind.Relative));
+
+        }
+        /// <summary>
+        /// Controls Sides button
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Sides_Click(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Navigate(new Uri("SideSelection.xaml", UriKind.Relative));
+        }
+    }
+}
