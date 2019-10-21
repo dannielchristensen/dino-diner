e2b55d466e22435e3607508a943a0ded2253a9c5
unknown <dannielchristensen1@gmail.com>
2019-10-20 23:49:18 -0500

    I have no idea how to use moq

--- MenuTest/OrderTest.cs
+++ MenuTest/OrderTest.cs
@@ -4,6 +4,9 @@ using System.Text;
 using Xunit;
 using Moq;
 using DinoDiner.Menu;
+using System.Collections.ObjectModel;
+using System.Linq;
+
 
 namespace MenuTest
 {
@@ -12,9 +15,25 @@ namespace MenuTest
         [Fact]
         public void ItemListAddsItems()
         {
-            var myMock = new Mock<IOrderItem>();
-            myMock.
-           
+
+            // I have no idea how to use moq lol
+
+            int num = 3;
+            var o = new Mock<Order>();
+            o.Setup(x => Enumerable.Repeat(Mock.Of<IOrderItem>(), num).ToList());
+            ObservableCollection<Mock<IOrderItem>> l = new ObservableCollection<Mock<IOrderItem>>();
+            var itemOne = new Mock<IOrderItem>();
+            itemOne.Setup(x => x.Price).Returns(5.67);
+            var itemTwo = new Mock<IOrderItem>();
+            itemTwo.Setup(x => x.Price).Returns(5.67);
+
+
+
+            
+
+
+
+
         }
     }
 }
